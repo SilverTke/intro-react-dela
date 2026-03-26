8096bd2af40db3d89879dd02ea67aefd00d62126
Sofía Maturana <sofimaturana830@gmail.com>
2026-03-26 10:48:35 -0300

    feat: agregar botones a CardPizza

--- src/components/CardPizza.jsx
+++ src/components/CardPizza.jsx
@@ -15,7 +15,10 @@ const CardPizza = (props) => (
 		<p className="font-bold text-green-700">
 			${props.price.toLocaleString("es-CL")}
 		</p>
-		<div className="flex"></div>
+		<div className="flex gap-4">
+			<button className="border-black border-2 rounded-md px-4">Ver más</button>
+			<button className="bg-black text-white rounded-md px-4">Añadir</button>
+		</div>
 	</article>
 );
 
