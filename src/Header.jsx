6dd7a11fbbc7eaed88be9981201d3a3abc86a110
Sofía Maturana <sofimaturana830@gmail.com>
2026-03-26 13:29:33 -0300

    fix: le puse dvw en vez de dvh al Header

--- src/Header.jsx
+++ src/Header.jsx
@@ -3,7 +3,7 @@ import headerUrl from "./assets/header.jpg";
 const Header = () => (
 	<header
 		style={{ "--img-url": `url(${headerUrl})` }}
-		className="h-[20dvw] w-full bg-[image:var(--img-url)] flex flex-col justify-center items-center text-white"
+		className="h-[20dvh] w-full bg-[image:var(--img-url)] flex flex-col justify-center items-center text-white"
 	>
 		<p className="text-5xl font-bold">Mamma mia!</p>
 		<p className="text-3xl">¡La mejor pizza!</p>
